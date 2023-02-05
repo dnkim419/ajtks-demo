d2b4d0dd67ec88fd01d8b2122cd24b4496bc9cb8
DAIN KIM <dnkim419@gmail.com>
2023-02-05 15:04:56 +0900

    style(Generator): css 적용

--- src/routes/Generator.tsx
+++ src/routes/Generator.tsx
@@ -1,313 +1,131 @@
 import styled from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
-
-const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0px;
-  gap: 197px;
-
-  position: absolute;
-  width: 289px;
-  height: 24px;
-  left: 16px;
-  top: 16px;
-`;
-
-const Logo = styled.img`
-  width: 68px;
-  height: 18px;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-`;
-
-const LogoText = styled.span`
-  position: absolute;
-  width: 68px;
-  height: 18px;
-  left: 0px;
-  top: 3px;
-
-  font-family: 'Carme';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 100%;
-  /* identical to box height, or 18px */
-
-  display: flex;
-  align-items: center;
-  text-align: center;
-
-  color: ${(props) => props.theme.textColor}; ;
-`;
-
-const LogoRectangle = styled.img`
-  position: absolute;
-  width: 10px;
-  height: 10px;
-  left: 51.6px;
-  top: 6.5px;
-
-  background: url(.png);
-  transform: matrix(-1, 0, 0, 1, 0, 0);
-`;
-
-const Close = styled.div`
-  width: 24px;
-  height: 24px;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 1;
-  flex-grow: 0;
-`;
-
-const CloseIcon = styled.img`
-  position: absolute;
-  left: 93.43%;
-  right: 1.73%;
-  top: 20.83%;
-  bottom: 20.83%;
-
-  background: ${(props) => props.theme.textColor};
-`;
-
-const InputBox = styled.div`
-  box-sizing: border-box;
-
-  /* Auto layout */
-
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  padding: 8px;
-  gap: 8px;
-
-  position: absolute;
-  width: 289px;
-  height: 30px;
-  left: 16px;
-  top: 56px;
-
-  border: 1px solid ${(props) => props.theme.textColor};
-  border-radius: 6px;
-`;
-
-const InputText = styled.span`
-  width: 273px;
-  height: 14px;
-
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 100%;
-  /* identical to box height, or 14px */
-
-  color: ${(props) => props.theme.textColor};
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-`;
+import { ReactComponent as LogoImg } from '../assets/images/Logo.svg';
+import { ReactComponent as CloseImg } from '../assets/images/Close.svg';
+import { ReactComponent as RefreshImg } from '../assets/images/Refresh.svg';
 
 const Container = styled.div`
+  width: 321px;
+  height: 457px;
+  padding: 16px;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Header = styled.header`
+  width: 289px;
+  height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BoundingBox = styled.div`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputBox = styled.input`
+  width: 289px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+  padding: 8px;
+  border: 1px solid ${(props) => props.theme.textColor};
+  border-radius: 6px;
+  font-family: 'Inter', sans-serif;
+  color: ${(props) => props.theme.textColor};
+  background-color: transparent;
+  /**
+  &::placeholder {
+    color: ${(props) => props.theme.textColor};
+  }
+  */
+`;
+
+const Images = styled.div`
   width: 287.6px;
   height: 288px;
+  margin-top: 16px;
+  display: grid;
+  column-gap: 7.6px;
+  row-gap: 8px;
+  grid-template-areas:
+    'Image Image'
+    'Image Image';
 `;
 
-const Image = styled.div`
-  position: absolute;
+const Image = styled.img`
   width: 140px;
   height: 140px;
-
-  background: url(.jpg), #d9d9d9;
-`;
-
-const Image1 = styled(Image)`
-  left: 17px;
-  top: 102px;
-`;
-
-const Image2 = styled(Image)`
-  left: 164.6px;
-  top: 102px;
-`;
-
-const Image3 = styled(Image)`
-  left: 17px;
-  top: 250px;
-`;
-
-const Image4 = styled(Image)`
-  left: 164.6px;
-  top: 250px;
 `;
 
 const UserControls = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  gap: 71px;
-
-  position: absolute;
   width: 289px;
   height: 28px;
-  left: 16px;
-  top: 413px;
-`;
-
-const Refresh = styled.div`
-  width: 24px;
-  height: 24px;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-`;
-
-const RefreshIcon = styled.img`
-  position: absolute;
-  left: 1.38%;
-  right: 93.08%;
-  top: 21.43%;
-  bottom: 21.43%;
-
-  background: ${(props) => props.theme.textColor};
+  margin-top: 23px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Buttons = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  gap: 8px;
-
   width: 194px;
   height: 28px;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 1;
-  flex-grow: 0;
-`;
-
-const Button = styled.button`
-  /* Auto layout */
-
   display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  padding: 8px 32px;
-  gap: 8px;
-
-  height: 28px;
-
-  border-radius: 5px;
-
-  /* Inside auto layout */
-
-  flex: none;
-
-  flex-grow: 0;
+  justify-content: space-between;
 `;
 
-const SaveButton = styled(Button)`
+const Btn = styled.button`
   width: 92px;
-  box-sizing: border-box;
-  background: ${(props) => props.theme.bgColor};
+  height: 28px;
   border: 1px solid ${(props) => props.theme.textColor};
-  order: 0;
-`;
-
-const ButtonText = styled.div`
-  height: 12px;
-
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 100%;
-  /* identical to box height, or 12px */
-
+  padding: 8px 32px;
+  border: 1px solid ${(props) => props.theme.textColor};
+  border-radius: 5px;
+  font-family: 'Inter', sans-serif;
+  color: ${(props) => props.theme.textColor};
+  background-color: transparent;
   display: flex;
+  justify-content: center;
   align-items: center;
-  text-align: center;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
 `;
 
-const SaveButtonText = styled(ButtonText)`
-  width: 28px;
+const SaveBtn = styled(Btn)``;
 
-  color: #ffffff;
-`;
-
-const CopyButton = styled(Button)`
-  width: 94px;
-  background: ${(props) => props.theme.textColor};
-  order: 1;
-`;
-
-const CopyButtonText = styled(ButtonText)`
-  width: 30px;
-
+const CopyBtn = styled(Btn)`
   color: #000000;
+  background-color: ${(props) => props.theme.textColor};
 `;
 
 function Generator() {
   return (
-    <>
+    <Container>
       <Header>
-        <Logo src=""></Logo>
-        <Close>
-          <CloseIcon src=""></CloseIcon>
-        </Close>
+        <LogoImg width="68px" height="18px" />
+        <BoundingBox>
+          <CloseImg width="14px" height="14px" />
+        </BoundingBox>
       </Header>
-      <InputBox>
-        <InputText></InputText>
-      </InputBox>
-
-      <Container>
-        <Image1></Image1>
-        <Image2></Image2>
-        <Image3></Image3>
-        <Image4></Image4>
-      </Container>
-
+      <InputBox placeholder="drag or paste" />
+      <Images>
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+      </Images>
       <UserControls>
-        <Refresh>
-          <RefreshIcon src=""></RefreshIcon>
-        </Refresh>
+        <BoundingBox>
+          <RefreshImg width="16px" height="16px" />
+        </BoundingBox>
         <Buttons>
-          <SaveButton>
-            <SaveButtonText>Span</SaveButtonText>
-          </SaveButton>
-          <CopyButton>
-            <CopyButtonText>Copy</CopyButtonText>
-          </CopyButton>
+          <SaveBtn>Save</SaveBtn>
+          <CopyBtn>Copy</CopyBtn>
         </Buttons>
       </UserControls>
-    </>
+    </Container>
   );
 }
 
